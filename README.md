eb8084757c1b452469e5b8b99bdd950163936b06
hearts <hearts.zhang@gmail.com>
2018-12-04 15:26:13 +0800

    - INITIALIZED

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
 # bert-feature
 
-[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
+![Python: 3.7](https://img.shields.io/badge/Python-3.7-brightgreen.svg)
+![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)
+![Ask Me Anything !](https://img.shields.io/badge/Ask%20me-anything-1abc9c.svg)
+![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)
+
+## Requirements
+
+- Anaconda or `Miniconda`
+- Python >=3.6 (and Python 2 is NOT Supported)
+- `PyTorch` nightly build
+- `TensorboardX`
+- pandas,  numpy...
+- `fire`
+- *pytorch-pretrained-BERT* (`pip install pytorch-pretrained-bert`)
+
+## Usage
+
+```bash
+python chinese.py --bert-model /repo/bert-models/ch-base \
+  --seq=',' \
+  --batch-n=64 </repo/svideo.name >svideo-768.nd 
+```
+
+### Read the nd like this
+
+```python
+import numpy as np
+vecs = np.fromfile('svideo-768.nd')
+vecs = vecs.reshape(-1, 768+1)  
+id, vec = vecs[:,0], vecs[:,1:]
+assert vec.shape[1]==768
+```
+
--- chinese.py
+++ chinese.py
@@ -1,25 +1,16 @@
-"""Extract pre-computed feature vectors from a PyTorch BERT model."""
-import argparse
-import collections
-import json
-import re
+"""Extract pre-computed feature vectors from a PyTorch BERT model.
+Some codes are copied from bert-pretrained-pytorch/examples."""
 import torch as t
+from sys import stdin, stderr, stdout
+from typing import Iterator, List
 from torch.utils.data import TensorDataset, DataLoader, SequentialSampler
 from pytorch_pretrained_bert.tokenization import BertTokenizer
 from pytorch_pretrained_bert.modeling import BertModel
-from sys import stderr
 from fire import Fire
 
 
 def log(*args, **kwargs):
   print(*args, file = stderr, **kwargs)
-
-
-class InputExample(object):
-  def __init__(self, unique_id, text_a, text_b):
-    self.unique_id = unique_id
-    self.text_a = text_a
-    self.text_b = text_b
 
 
 class InputFeatures(object):
@@ -33,205 +24,160 @@ class InputFeatures(object):
     self.input_type_ids = input_type_ids
 
 
-def convert_examples_to_features(examples, seq_length, tokenizer):
-  """Loads a data file into a list of `InputBatch`s."""
+def convert_example_to_features(uid: int, text_a: str, text_b: str,
+                                seq_length: int,
+                                tokenizer: BertTokenizer, ) -> InputFeatures:
+  tokens_a = tokenizer.tokenize(text_a)
+  tokens_b = text_b and tokenizer.tokenize(text_b) or []
 
-  features = []
-  for (ex_index, example) in enumerate(examples):
-    tokens_a = tokenizer.tokenize(example.text_a)
+  _truncate_seq(tokens_a, tokens_b, seq_length)
 
-    tokens_b = None
-    if example.text_b:
-      tokens_b = tokenizer.tokenize(example.text_b)
+  # The convention in BERT is:
+  # (a) For sequence pairs:
+  #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
+  #  type_ids: 0   0  0    0    0     0       0 0    1  1  1  1   1 1
+  # (b) For single sequences:
+  #  tokens:   [CLS] the dog is hairy . [SEP]
+  #  type_ids: 0   0   0   0  0     0 0
+  #
+  # Where "type_ids" are used to indicate whether this is the first
+  # sequence or the second sequence. The embedding vectors for `type=0` and
+  # `type=1` were learned during pre-training and are added to the wordpiece
+  # embedding vector (and position vector). This is not *strictly* necessary
+  # since the [SEP] token unambiguously separates the sequences, but it makes
+  # it easier for the model to learn the concept of sequences.
+  #
+  # For classification tasks, the first vector (corresponding to [CLS]) is
+  # used as as the "sentence vector". Note that this only makes sense because
+  # the entire model is fine-tuned.
+  tokens = ["[CLS]"] + tokens_a + ["[SEP]"]
+  input_type_ids = [0] * (len(tokens_a) + 2)
 
-    if tokens_b:
-      # Modifies `tokens_a` and `tokens_b` in place so that the total
-      # length is less than the specified length.
-      # Account for [CLS], [SEP], [SEP] with "- 3"
-      _truncate_seq_pair(tokens_a, tokens_b, seq_length - 3)
-    else:
-      # Account for [CLS] and [SEP] with "- 2"
-      if len(tokens_a) > seq_length - 2:
-        tokens_a = tokens_a[0:(seq_length - 2)]
+  if tokens_b:
+    tokens = tokens + tokens_b + ["[SEP]"]
+    input_type_ids = [1] * (len(tokens_b) + 1)
 
-    # The convention in BERT is:
-    # (a) For sequence pairs:
-    #  tokens:   [CLS] is this jack ##son ##ville ? [SEP] no it is not . [SEP]
-    #  type_ids: 0   0  0    0    0     0       0 0    1  1  1  1   1 1
-    # (b) For single sequences:
-    #  tokens:   [CLS] the dog is hairy . [SEP]
-    #  type_ids: 0   0   0   0  0     0 0
-    #
-    # Where "type_ids" are used to indicate whether this is the first
-    # sequence or the second sequence. The embedding vectors for `type=0` and
-    # `type=1` were learned during pre-training and are added to the wordpiece
-    # embedding vector (and position vector). This is not *strictly* necessary
-    # since the [SEP] token unambigiously separates the sequences, but it makes
-    # it easier for the model to learn the concept of sequences.
-    #
-    # For classification tasks, the first vector (corresponding to [CLS]) is
-    # used as as the "sentence vector". Note that this only makes sense because
-    # the entire model is fine-tuned.
-    tokens = []
-    input_type_ids = []
-    tokens.append("[CLS]")
-    input_type_ids.append(0)
-    for token in tokens_a:
-      tokens.append(token)
-      input_type_ids.append(0)
-    tokens.append("[SEP]")
-    input_type_ids.append(0)
+  input_ids = tokenizer.convert_tokens_to_ids(tokens)
 
-    if tokens_b:
-      for token in tokens_b:
-        tokens.append(token)
-        input_type_ids.append(1)
-      tokens.append("[SEP]")
-      input_type_ids.append(1)
+  # The mask has 1 for real tokens and 0 for padding tokens. Only real
+  # tokens are attended to.
+  input_mask = [1] * len(input_ids)
 
-    input_ids = tokenizer.convert_tokens_to_ids(tokens)
+  # Zero-pad up to the sequence length.
+  x = [0] * (seq_length - len(input_ids))
+  input_ids += x
+  input_type_ids += x
+  input_mask += x
 
-    # The mask has 1 for real tokens and 0 for padding tokens. Only real
-    # tokens are attended to.
-    input_mask = [1] * len(input_ids)
+  assert len(input_ids) == seq_length
+  assert len(input_mask) == seq_length
+  assert len(input_type_ids) == seq_length
 
-    # Zero-pad up to the sequence length.
-    while len(input_ids) < seq_length:
-      input_ids.append(0)
-      input_mask.append(0)
-      input_type_ids.append(0)
-
-    assert len(input_ids) == seq_length
-    assert len(input_mask) == seq_length
-    assert len(input_type_ids) == seq_length
-
-    if ex_index < 5:
-      log("*** Example ***")
-      log("unique_id: %s" % example.unique_id)
-      log("tokens: %s" % " ".join([str(x) for x in tokens]))
-      log("input_ids: %s" % " ".join([str(x) for x in input_ids]))
-      log("input_mask: %s" % " ".join([str(x) for x in input_mask]))
-      log("input_type_ids: %s" % " ".join([str(x) for x in input_type_ids]))
-
-    features.append(InputFeatures(unique_id = example.unique_id,
-                                  tokens = tokens,
-                                  input_ids = input_ids,
-                                  input_mask = input_mask,
-                                  input_type_ids = input_type_ids))
-  return features
+  return InputFeatures(unique_id = uid,
+                       tokens = tokens,
+                       input_ids = input_ids,
+                       input_mask = input_mask,
+                       input_type_ids = input_type_ids)
 
 
-def _truncate_seq_pair(tokens_a, tokens_b, max_length):
+def _truncate_seq(a: List[str], b: List[str], max_length: int) -> None:
   """Truncates a sequence pair in place to the maximum length."""
-
   # This is a simple heuristic which will always truncate the longer sequence
   # one token at a time. This makes more sense than truncating an equal percent
   # of tokens from each, since if one sequence is very short then each token
   # that's truncated likely contains more information than a longer sequence.
-  while True:
-    total_length = len(tokens_a) + len(tokens_b)
-    if total_length <= max_length:
-      break
-    if len(tokens_a) > len(tokens_b):
-      tokens_a.pop()
-    else:
-      tokens_b.pop()
+  # Modifies `tokens_a` and `tokens_b` in place so that the total
+  # length is less than the specified length.
+
+  # Account for [CLS], [SEP], [SEP] with "- 3" if b exists
+  # Account for [CLS] and [SEP] with "- 2" if b not exists
+  max_length = b and (max_length - 3) or (max_length - 2)
+  b = b or []  # for suppress len(b)'s exception
+  while max_length < len(a) + len(b):
+    x = len(a) >= len(b) and a or b
+    x.pop()
 
 
-def read_examples(input_file):
-  """Read a list of `InputExample`s from an input file."""
-  examples = []
-  unique_id = 0
-  with open(input_file, "r") as reader:
-    while True:
-      line = reader.readline()
-      if not line:
-        break
-      line = line.strip()
-      text_a = None
-      text_b = None
-      m = re.match(r"^(.*) \|\|\| (.*)$", line)
-      if m is None:
-        text_a = line
-      else:
-        text_a = m.group(1)
-        text_b = m.group(2)
-      examples.append(InputExample(unique_id = unique_id, text_a = text_a, text_b = text_b))
-      unique_id += 1
-  return examples
+def each_line(max_seq_len: int,
+              batch_n: int,
+              sep: str,
+              tokenizer) -> Iterator[List[InputFeatures]]:
+  batch = []
+
+  for line in stdin:
+    line = line.strip()
+    if not line:
+      continue
+    lid, line = line.split(sep, 1)
+    ab = line.split('|||', 1)
+    text_a = ab[0]
+    text_b = len(ab) == 2 and ab[1] or None
+    # ie = InputExample(uid, text_a, text_b)
+    f = convert_example_to_features(int(lid), text_a, text_b, max_seq_len, tokenizer)
+
+    batch.append(f)
+    if len(batch) >= batch_n:
+      yield batch
+      batch = []
+  if batch:
+    yield batch
 
 
-def main():
-  parser = argparse.ArgumentParser()
+def main(bert_model: str = '/repo/bert-models/ch-base',
+         sep: str = ',',
+         with_lower_case: bool = True,
+         layer: int = -2,
+         max_seq_length: int = 128,
+         batch_n: int = 32,
+         without_cuda: bool = False) -> None:
+  """bert-model: bert-base-uncased, bert-large-uncased,
+                 bert-base-cased, bert-base-multilingual, bert-base-chinese.
+  with-lower-case: Set this flag if you are using an uncased model.
+  layer: -1, -2, -3, -4...-12, we just use the last layer
+  max-seq-length: The maximum total input sequence length after
+                  WordPiece tokenization. Sequences longer
+                  than this will be truncated, and sequences shorter than
+                  this will be padded.
+  batch-n: Batch size for predictions.
+  without-cuda: Whether not to use CUDA.  """
 
-  # Required parameters
-  parser.add_argument("--input_file", default = 'sample.txt', type = str, )
-  parser.add_argument("--output_file", default = 'sample.result', type = str, )
-  parser.add_argument("--bert_model", default = '/repo/bert-models/ch-base',
-                      type = str,
-                      help = "Bert pre-trained model selected in the list: bert-base-uncased, "
-                             "bert-large-uncased, bert-base-cased, bert-base-multilingual, "
-                             "bert-base-chinese.", )
-
-  # Other parameters
-  parser.add_argument("--do_lower_case", default = False, action = 'store_true',
-                      help = "Set this flag if you are using an uncased model.")
-  parser.add_argument("--layers", default = "-2", type = str)
-  parser.add_argument("--max_seq_length", default = 128, type = int,
-                      help = "The maximum total input sequence length after "
-                             "WordPiece tokenization. Sequences longer "
-                             "than this will be truncated, and sequences shorter than "
-                             "this will be padded.")
-  parser.add_argument("--batch_size", default = 32, type = int,
-                      help = "Batch size for predictions.")
-  parser.add_argument("--without_cuda",
-                      default = False,
-                      action = 'store_true',
-                      help = "Whether not to use CUDA when available")
-
-  args = parser.parse_args()
-  args.without_cuda = not t.cuda.is_available() and args.without_cuda
-  device = t.device("cuda" if args.without_cuda else "cpu")
+  without_cuda = without_cuda or not t.cuda.is_available()
+  device = t.device(without_cuda and "cpu" or "cuda")
   n_gpu = t.cuda.device_count()
 
-  log("device: {} n_gpu: {} ".format(device, n_gpu))
+  log(f"device: {device} n_gpu: {n_gpu} ")
 
-  layer_indexes = [int(x) for x in args.layers.split(",")]
+  tokenizer = BertTokenizer.from_pretrained(bert_model, do_lower_case = with_lower_case)
 
-  tokenizer = BertTokenizer.from_pretrained(args.bert_model, do_lower_case = args.do_lower_case)
+  for features in each_line(max_seq_length, batch_n, sep, tokenizer):
+    unique_id_to_feature = {}
+    for feature in features:
+      unique_id_to_feature[feature.unique_id] = feature
 
-  examples = read_examples(args.input_file)
+    model = BertModel.from_pretrained(bert_model)
+    model.to(device)
 
-  features = convert_examples_to_features(examples = examples, seq_length = args.max_seq_length,
-                                          tokenizer = tokenizer, )
+    all_input_ids = t.tensor([f.input_ids for f in features], dtype = t.long)
+    all_input_mask = t.tensor([f.input_mask for f in features], dtype = t.long)
+    # all_example_index = t.arange(all_input_ids.size(0), dtype = t.long)
+    all_example_index = t.tensor([f.unique_id for f in features], dtype = t.long)
 
-  unique_id_to_feature = {}
-  for feature in features:
-    unique_id_to_feature[feature.unique_id] = feature
+    eval_data = TensorDataset(all_input_ids, all_input_mask, all_example_index)
+    eval_sampler = SequentialSampler(eval_data)
 
-  model = BertModel.from_pretrained(args.bert_model)
-  model.to(device)
+    eval_dataloader = DataLoader(eval_data, sampler = eval_sampler, batch_size = batch_n)
 
-  all_input_ids = t.tensor([f.input_ids for f in features], dtype = t.long)
-  all_input_mask = t.tensor([f.input_mask for f in features], dtype = t.long)
-  all_example_index = t.arange(all_input_ids.size(0), dtype = t.long)
-
-  eval_data = TensorDataset(all_input_ids, all_input_mask, all_example_index)
-  eval_sampler = SequentialSampler(eval_data)
-
-  eval_dataloader = DataLoader(eval_data, sampler = eval_sampler, batch_size = args.batch_size)
-
-  model.eval()
-  with open(args.output_file, "w", encoding = 'utf-8') as writer:
-    for input_ids, input_mask, example_indices in eval_dataloader:
+    model.eval()
+    for input_ids, input_mask, uids in eval_dataloader:
       input_ids = input_ids.to(device)
       input_mask = input_mask.to(device)
 
       all_encoder_layers, _ = model(input_ids, token_type_ids = None, attention_mask = input_mask)
-      all_encoder_layers = all_encoder_layers[-2]
-      pooled = all_encoder_layers.mean(dim = 1)
-      print(pooled.shape)
+      all_encoder_layers = all_encoder_layers[layer]
+      pooled = all_encoder_layers.mean(dim = 1)  # 平均池化
+      o = t.cat([uids.reshape(-1, 1).float(), pooled], dim = 1)
+      o.detach().numpy().tofile(stdout)
 
 
 if __name__ == "__main__":
-  main()
+  Fire(main)
