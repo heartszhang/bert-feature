e007dc44ba10397b48817dd6872e88b06854d4a1
hearts <hearts.zhang@gmail.com>
2018-12-07 12:23:19 +0800

    - UPDATED 注释部分代码

--- match.py
+++ match.py
@@ -13,20 +13,25 @@ def main(vec: str,
          sample_n: int = 100, ) -> None:
   names = pd.read_csv(name, header = None, names = ['vid', 'name'])
   data = np.loadtxt(vec)
+  # 我们的csv不是标准方式写入的，保不齐出现两个shape不对应的情况
   assert data.shape[0] == names.shape[0]
-  vids = data[:, 0].astype(int)
+  # vids = data[:, 0].astype(int), 程序中第一列是video-id，但我们这个程序没有用这个
   vecs = data[:, 1:].astype(np.float32)
   dim = vecs.shape[1]
 
-  quantizer = faiss.IndexFlatL2(dim)
+  quantizer = faiss.IndexFlatL2(dim)  # 我们用内积量化向量距离就可以了, 也应该尝试下别的
+  # 对向量集合进行索引
+  # 我不知道nlist应该多少合适，选了和CPU差不多的一个数
   index = faiss.IndexIVFFlat(quantizer, dim, nlist, faiss.METRIC_L2)
   index.train(vecs)
   index.add(vecs)
 
+  # 探查次数越多就越准确，时间也翻倍
   index.nprobe = nprobe
   _, i = index.search(vecs, k)
   to = Path(name).with_suffix(f'.{k}-search').resolve()
   np.savetxt(to, i, fmt = '%d')
+  # 肉眼观察一些数据
   samples = np.random.choice(len(i), sample_n)
   for sample in i[samples]:
     g = names.iloc[sample]
